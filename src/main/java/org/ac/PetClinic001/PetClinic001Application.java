f674f5bed25ca5494a83ad114fc4b2d0bc0436b1
Eldurlin <adam.cakala@gmail.com>
2020-08-05 11:43:03 +0200

    Initial project add

---
+++ src/main/java/org/ac/PetClinic001/PetClinic001Application.java
@@ -0,0 +1,13 @@
+package org.ac.PetClinic001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinic001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinic001Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/ac/PetClinic001/PetClinic001ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ac.PetClinic001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinic001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
