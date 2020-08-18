ddb18360e2025451fcc3ee3b2502e9f53d3b8806
Eldurlin <adam.cakala@gmail.com>
2020-08-18 18:02:14 +0200

    impl map based services, closes #5

---
+++ PetClinic001Data/src/main/java/org/ac/PetClinic001/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package org.ac.PetClinic001.services.map;
+
+import java.util.Set;
+
+import org.ac.PetClinic001.model.Pet;
+import org.ac.PetClinic001.services.CrudService;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
