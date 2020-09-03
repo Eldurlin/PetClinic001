293f6c8a8cf4aa8c2ebbb28f1f4ed99efcac9825
Eldurlin <adam.cakala@gmail.com>
2020-09-03 10:26:56 +0200

    updated vet to use JPA, closes #36

--- PetClinic001Data/src/main/java/org/ac/PetClinic001/model/Speciality.java
+++ PetClinic001Data/src/main/java/org/ac/PetClinic001/model/Speciality.java
@@ -1,6 +1,13 @@
 package org.ac.PetClinic001.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "speciality")
 public class Speciality extends BaseEntity {
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
