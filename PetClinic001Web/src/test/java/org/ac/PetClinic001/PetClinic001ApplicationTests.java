b8205ae6cc415d51e76bb426ef5fd9b07b12539c
Eldurlin <adam.cakala@gmail.com>
2020-09-14 10:13:15 +0200

    added tests, closes #49

--- PetClinic001Web/src/test/java/org/ac/PetClinic001/PetClinic001ApplicationTests.java
+++ PetClinic001Web/src/test/java/org/ac/PetClinic001/PetClinic001ApplicationTests.java
@@ -1,10 +1,13 @@
 package org.ac.PetClinic001;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-class PetClinic001ApplicationTests {
+public class PetClinic001ApplicationTests {
 
 	@Test
 	void contextLoads() {
